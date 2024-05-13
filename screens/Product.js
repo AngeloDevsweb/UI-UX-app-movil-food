57c7c4316b6bc677e285b3c39c80f6c982b2937a
Angelo <gabriel_propio95@hotmail.com>
2024-05-13 13:14:12 -0400

    pantall productStyle completa, arreglo de errores en la imagen, y creacion de apis de forma local dentro del documento del proyecto para simular peticion de una endpoint

--- screens/Product.js
+++ screens/Product.js
@@ -1,37 +1,66 @@
-import React from 'react'
-import { Text, View, SafeAreaView, StatusBar, FlatList, TouchableOpacity } from 'react-native'
-import { productStyle } from '../styles/productStyle'
-import { etiquetas } from '../Apis/etiquetas'
+import React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { productStyle } from "../styles/productStyle";
+import { etiquetas } from "../Apis/etiquetas";
+import { productos } from "../Apis/productos";
 
 export function Product() {
-    const renderItem = ({ item }) => (
-        <TouchableOpacity style={productStyle.boxTag}>
-            <Text style={productStyle.textTag} >{item.title}</Text>
-        </TouchableOpacity>
-      );
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={productStyle.boxTag}>
+      <Text style={productStyle.textTag}>{item.title}</Text>
+    </TouchableOpacity>
+  );
 
+  const productList = ({ item }) => {
+    //console.log(item);
     return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: "#FFFFFF" }}> 
-        <StatusBar
+      <TouchableOpacity>
+      <View style={productStyle.tarjeta}>
+        <Image source={{ uri: item.imagen }} style={productStyle.imagen} />
+        <View style={productStyle.contenido}>
+          <Text style={productStyle.nombre}>{item.nombre}</Text>
+          <Text style={productStyle.descripcion}>{item.etiqueta}</Text>
+          <Text style={productStyle.precio}>Precio: ${item.precio}</Text>
+        </View>
+      </View>
+      </TouchableOpacity>
+    );
+  };
+  
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
+      <StatusBar
         barStyle="dark-content"
         translucent={true}
         backgroundColor="transparent"
       />
-        <View style={productStyle.ContainerTags}>
-           
-            <FlatList 
-                data={etiquetas}
-                keyExtractor={(item)=>item.id}
-                renderItem={renderItem}
-                horizontal
-            />
-        </View>
+      <View style={productStyle.ContainerTags}>
+        <FlatList
+          data={etiquetas}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+          horizontal
+        />
+      </View>
 
-        <View style={productStyle.containerList}>
-
-        </View>
-      </SafeAreaView>
-    )
+      <View style={productStyle.containerList}>
+        <FlatList
+          data={productos}
+          keyExtractor={(item) => item.id}
+          renderItem={productList}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
 
-export default Product
+export default Product;
